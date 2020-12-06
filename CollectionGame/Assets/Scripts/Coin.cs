d4bf486af8ceadd4759d4150a719da4fdcb7e2fe
mrcrambo <kadirov.il.crambo@gmail.com>
2020-12-07 00:37:39 +0300

    scripting added

---
+++ CollectionGame/Assets/Scripts/Coin.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    
+    public static int coinCount = 0;
+
+    void Start()
+    {
+        ++coinCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+        if (col.CompareTag("Player"))
+            Destroy(gameObject);
+    }
+
+    void OnDestroy()
+    {
+        --coinCount;
+
+        if (coinCount <= 0)
+        {
+
+        }
+    }
+}
