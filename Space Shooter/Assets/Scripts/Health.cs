76b886d8cd6f1e9cf9ca1cdcd296f12a5619c20b
mrcrambo <kadirov.il.crambo@gmail.com>
2020-12-14 22:29:22 +0300

    health class added

---
+++ Space Shooter/Assets/Scripts/Health.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Health : MonoBehaviour
+{
+    public GameObject deathParticlesPrefab = null;
+    public bool shoudDestrouOnDeath = true;
+    [SerializeField] private float _HealthPoints = 100f;
+
+    public float HealthPoints
+    {
+        get
+        {
+            return _HealthPoints;
+        }
+
+        set
+        {
+            _HealthPoints = value;
+
+            if (_HealthPoints <= 0)
+            {
+                SendMessage("Die", SendMessageOptions.DontRequireReceiver);
+                if (deathParticlesPrefab != null)
+                {
+                    Instantiate(deathParticlesPrefab, transform.position, transform.rotation);
+                }
+                if (shoudDestrouOnDeath)
+                {
+                    Destroy(gameObject);
+                }
+            }
+        }
+    }
+}
