f6c291b16c1a12f2a692b9d171a6f7316d960da2
mrcrambo <kadirov.il.crambo@gmail.com>
2020-12-17 20:50:37 +0300

    random enemies spawning added

---
+++ Space Shooter/Assets/Scripts/DamageDealer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamageDealer : MonoBehaviour
+{
+    public float damageRate = 10f;
+
+    /// <summary>
+    /// OnTriggerStay is called once per frame for every Collider other
+    /// that is touching the trigger.
+    /// </summary>
+    /// <param name="other">The other Collider involved in this collision.</param>
+    void OnTriggerStay(Collider other)
+    {
+        Health h = other.gameObject.GetComponent<Health>();
+        if (h == null)
+        {
+            return;
+        }
+        h.HealthPoints -= damageRate * Time.deltaTime;
+    }
+}
---
+++ Space Shooter/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    public float maxRadius = 1f;
+    public float interval = 3f;
+    public GameObject objToSpawn = null;
+    private Transform origin = null;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        origin = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {
+        InvokeRepeating("Spawn", 0f, interval);
+    }
+
+    void Spawn()
+    {
+        if (origin == null)
+        {
+            return;
+        }
+
+        Vector3 spawnPos = origin.position + Random.onUnitSphere * maxRadius;
+        spawnPos = new Vector3(spawnPos.x, 0f, spawnPos.z);
+        Instantiate(objToSpawn, spawnPos, Quaternion.identity);
+    }
+}
