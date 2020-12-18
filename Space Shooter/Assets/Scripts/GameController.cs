08c063d6a5b5ed6fd7f389e859c3ca2068aad68a
mrcrambo <kadirov.il.crambo@gmail.com>
2020-12-18 15:37:40 +0300

    space shooter finished with audio, score, etc

---
+++ Space Shooter/Assets/Scripts/GameController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+    public static GameController GameControllerInstance = null;
+
+    public static int score;
+    public string scorePrefix = string.Empty;
+    public Text scoreText = null;
+    public Text gameOverText = null;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        GameControllerInstance = this;
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        if (scoreText != null)
+        {
+            scoreText.text = scorePrefix + score.ToString();
+        }
+    }
+
+    public static void GameOver()
+    {
+        if (GameControllerInstance.gameOverText != null)
+        {
+            GameControllerInstance.gameOverText.gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Space Shooter/Assets/Scripts/ScoreOnDestroy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreOnDestroy : MonoBehaviour
+{
+    
+    public int score = 1;
+
+    /// <summary>
+    /// This function is called when the MonoBehaviour will be destroyed.
+    /// </summary>
+    void OnDestroy()
+    {
+        GameController.score += score;
+    }
+}
