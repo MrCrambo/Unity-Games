4a768ec7d504b99f0c39072dbbc1ec96d1e58d08
mrcrambo <kadirov.il.crambo@gmail.com>
2020-12-21 14:45:49 +0300

    canvas with health bar added

---
+++ Adventure 2D/Assets/Scripts/HealthBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthBar : MonoBehaviour
+{
+    public float healthDecreaseSpeed = 10f;
+    public RectTransform thisTranfosrm = null;
+
+    /// <summary>
+    /// Awake is called when the script instance is being loaded.
+    /// </summary>
+    void Awake()
+    {
+        thisTranfosrm = GetComponent<RectTransform>();
+    }
+
+    /// <summary>
+    /// Update is called every frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void Update()
+    {
+        float healthUpdate = 0f;
+        if (PlayerController.playerInstance != null)
+        {
+            healthUpdate = Mathf.MoveTowards(thisTranfosrm.rect.width, PlayerController.Health, healthDecreaseSpeed);
+        }
+
+        thisTranfosrm.sizeDelta = new Vector2(Mathf.Clamp(healthUpdate, 0, 100), thisTranfosrm.sizeDelta.y);
+    }
+}
