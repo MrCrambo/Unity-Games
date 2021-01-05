686d903ba861e117e6d056c4ddf88eadcc9c02e9
mrcrambo <kadirov.il.crambo@gmail.com>
2021-01-05 14:14:24 +0300

    destination following added

---
+++ NPC AI/Assets/Scripts/FollowDestination.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class FollowDestination : MonoBehaviour
+{
+    public Transform destination = null;
+    private NavMeshAgent agent = null;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        agent.SetDestination(destination.position);    
+    }
+}
