e7babb87f14e496e2e303f12cb5db9fcf941bd34
mrcrambo <kadirov.il.crambo@gmail.com>
2021-01-07 12:36:48 +0300

    moving animations added

---
+++ NPC AI/Assets/Scripts/AnimationController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent), typeof(Animator))]
+public class AnimationController : MonoBehaviour
+{
+    public float velocity = 0.1f;
+    public string runName = "Run";
+    public string speedName = "Speed";
+    
+    private float maxSpeed;
+
+    private NavMeshAgent thisAgent = null;
+    private Animator thisAnimator = null;
+
+    private void Awake()
+    {
+        thisAgent = GetComponent<NavMeshAgent>();
+        thisAnimator = GetComponent<Animator>();
+        maxSpeed = thisAgent.speed;
+    }
+
+    private void Update()
+    {
+        thisAnimator.SetBool(runName, thisAgent.velocity.magnitude > velocity); 
+        thisAnimator.SetFloat(speedName, thisAgent.velocity.magnitude / maxSpeed);   
+    }
+}
