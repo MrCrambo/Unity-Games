78d6d70cba3b6ad46fd74e9006772606a683a842
mrcrambo <kadirov.il.crambo@gmail.com>
2021-01-07 17:30:05 +0300

    player controller and camera added

---
+++ NPC AI/Assets/Scripts/CameraLook.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public float lookSpeed = 3.0f;
+    public Transform body = null;
+    public Camera lookCamera = null;
+
+    private Vector2 rotation = Vector2.zero;
+
+    private void Update()
+    {
+        rotation.y += Input.GetAxis("Mouse X");
+        rotation.x -= Input.GetAxis("Mouse Y");
+        Vector2 rotateStep = rotation * lookSpeed;
+        body.eulerAngles = new Vector2(0, rotateStep.y);
+        lookCamera.transform.localRotation = Quaternion.Euler(rotateStep.x, 0, 0);
+    }
+}
---
+++ NPC AI/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 3.0f;
+
+    private void Update()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        Vector3 translation = new Vector3(horiz, 0, vert);
+        translation *= speed * Time.deltaTime;
+        transform.Translate(translation);
+    }
+}
